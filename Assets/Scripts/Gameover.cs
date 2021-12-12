2a1a1084fafcbedd73b473c0932ef6279b0f59dc
Indraneel Dutta <meneel2000@gmail.com>
2021-12-12 12:29:05 +0530

    First commit

---
+++ Assets/Scripts/Gameover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gameover : MonoBehaviour
+{
+
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	void OnGUI()
+	{
+
+		//if retry button is pressed load scene 0 the game
+		GUI.contentColor = Color.red;
+		if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 4 + 150, 150, 40), "Retry?"))
+		{
+			Application.LoadLevel(0);
+		}
+		//and quit button
+		if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 4 + 200, 150, 40), "Quit"))
+		{
+			Application.Quit();
+		}
+	}
+
+}
---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/Player_Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player_Movement : MonoBehaviour
+{
+    public GameObject playerDestination;
+    NavMeshAgent theAgent;
+    void Start()
+    {
+        theAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        theAgent.SetDestination(playerDestination.transform.position);
+    }
+}
